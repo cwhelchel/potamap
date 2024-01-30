03591997d3e72793beadb199a238a298569e0b99
Cainan Whelchel <c.whelchel@gmail.com>
2024-01-30 11:58:00 -0500

    Add several features: - UI to show / hide the labels for: activated, hunted, or unknown (not hunted or activated) - UI to set the map marker label color

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import os
 import json
 import PIL.ImageTk
 import webbrowser
+import re
+from idlelib.tooltip import Hovertip
 
 from stats import PotaStats
 
@@ -18,7 +20,8 @@ VERSION = '0.0.1'
 locations = []
 locations2 = {}
 loc_coords = {}
-default_cfg = {'location': 'US-GA', 'init_x': 33.0406, 'init_y': -83.6431, 'display_opts': 0}
+default_cfg = {'location': 'US-GA', 'init_x': 33.0406, 'init_y': -83.6431, 'display_opts': 0,
+               'show_actx_lbl': True, 'show_hunt_lbl': False, 'show_unkn_lbl': False, 'text_color': '#6d6af7'}
 config = {}
 
 #
@@ -103,6 +106,16 @@ def read_parks(window):
         for d in home_data:
             window.create_park_marker(d)
 
+def check_color_input(val : str) -> str:
+    '''
+    Checks if the input is a valid hex color. If invalid returns a default
+    '''
+    match = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', val)
+    if match: 
+        return val
+    else:
+        return "#6d6af7" # original default val
+
 
 class PotaMapRoot(tkinter.Tk):
     '''Our main GUI class to display the POTA map'''
@@ -117,6 +130,10 @@ class PotaMapRoot(tkinter.Tk):
         self.state('zoomed')
         self.title("POTA MAP")
         self.loc_var = StringVar()
+        self.color_var = StringVar()
+        self.show_hunt_var = IntVar()
+        self.show_actx_var = IntVar()
+        self.show_unkn_var = IntVar() # these are for the yellow dots
 
         self.create_widgets()
 
@@ -124,6 +141,10 @@ class PotaMapRoot(tkinter.Tk):
         self.map.set_position(config['init_x'], config['init_y'])
         self.map.set_zoom(7)
         self.loc_var.set(config['location'])
+        self.show_hunt_var.set(1 if config['show_hunt_lbl'] else 0)
+        self.show_actx_var.set(1 if config['show_actx_lbl'] else 0)
+        self.show_unkn_var.set(1 if config['show_unkn_lbl'] else 0)
+        self.color_var.set(config['text_color'])
 
     def create_widgets(self):
         x = 1200
@@ -157,13 +178,28 @@ class PotaMapRoot(tkinter.Tk):
         self.lbl_displayopts.pack(side='left')
     
         self.combo_displayopts_vals = ['Full Park Name', 'Park# Only']
-        #global config
-        print(config['display_opts'])
         self.combo_displayopts = ttk.Combobox(self.combo_frame2, values=self.combo_displayopts_vals)
         self.combo_displayopts.current(config['display_opts'])
         self.combo_displayopts.pack(side='top', anchor='w')
         self.combo_displayopts.bind("<<ComboboxSelected>>", self.combo_displayopts_callback)
         self.combo_displayopts.bind("<Return>", self.combo_displayopts_enter)
+
+        self.lbl_opts_frame = ttk.Frame(self.left_frame, padding=10)
+        self.lbl_opts_frame.pack(side='top', fill='x')
+        self.chk_show_hunt = ttk.Checkbutton(self.lbl_opts_frame, text="Hunt Labels", variable=self.show_hunt_var, command=self.handle_check)
+        self.chk_show_hunt.pack(side='top', anchor='w')
+        self.chk_show_actx = ttk.Checkbutton(self.lbl_opts_frame, text="Activation Labels", variable=self.show_actx_var, command=self.handle_check)
+        self.chk_show_actx.pack(side='top', anchor='w')
+        self.chk_show_unkn = ttk.Checkbutton(self.lbl_opts_frame, text="Unknown Labels", variable=self.show_unkn_var, command=self.handle_check)
+        self.chk_show_unkn.pack(side='top', anchor='w')
+        self.tt = Hovertip(self.chk_show_unkn, 'For the un-hunted un-activated parks')
+        
+        self.lbl_color = ttk.Label(self.lbl_opts_frame, text="Color:")
+        self.lbl_color.pack(side='left')
+        self.color_textbox = ttk.Entry(self.lbl_opts_frame, text="Color:", textvariable=self.color_var)
+        self.color_textbox.bind("<Return>", self.accept_color_input)
+        self.color_textbox.bind("<FocusOut>", self.accept_color_input)
+        self.color_textbox.pack(side='top')
 
         self.loc_info_frame = ttk.Frame(self.left_frame, padding=10)
         self.loc_info_frame.pack(side='top', fill='x')
@@ -174,8 +210,6 @@ class PotaMapRoot(tkinter.Tk):
             self.loc_info_frame, text="Open Stats Page", command=self.open_browser)
         self.browse_btn.pack(side='bottom')
 
-        
-        
         self.right_frame = ttk.Frame(self, padding=10)
         self.right_frame.pack(side='right')
         self.map = tkintermapview.TkinterMapView(
@@ -199,32 +233,34 @@ class PotaMapRoot(tkinter.Tk):
             text = f"{marker.data['reference']} - {marker.data['name']}"
             self.park_lbl.configure(text=text)
 
+        def get_displayopt_text(display_key: str):
+            if not config[display_key]: return ""
+
+            if config['display_opts'] == 1: # Option 1: Park# Only
+                text = f"{ref}"
+            else: # Default/Option 0 - Full Park Name
+                text = f"{ref} {park['name']}"
+
+            return text
+        
         ref = park['reference']
         data = park
 
         actxed = self.stats.has_activated(ref)
         hunted = self.stats.has_hunted(ref)
 
-        def get_displayopt_text():
-                if config['display_opts'] == 1: # Option 1: Park# Only
-                    text = f"{ref}"
-                else: # Default/Option 0 - Full Park Name
-                    text = f"{ref} {park['name']}"
-
-                return text
-
         if actxed and hunted:
             i = PIL.ImageTk.PhotoImage(file="cd.png")
-            name = get_displayopt_text()
+            name = get_displayopt_text('show_actx_lbl')
         elif actxed:
             i = PIL.ImageTk.PhotoImage(file="bd.png")
-            name = get_displayopt_text()
+            name = get_displayopt_text('show_actx_lbl')
         elif hunted:
             i = PIL.ImageTk.PhotoImage(file="gd.png")
-            name = ""
+            name = get_displayopt_text('show_hunt_lbl')
         else:
             i = PIL.ImageTk.PhotoImage(file="yd.png")
-            name = ""
+            name = get_displayopt_text('show_unkn_lbl')
 
         self.map.set_marker(
             park['latitude'],
@@ -232,7 +268,7 @@ class PotaMapRoot(tkinter.Tk):
             text=name,
             font="helvetica 12 bold",
             icon=i,
-            text_color="#6d6af7",
+            text_color=check_color_input(config["text_color"]),
             data=data,
             command=cmd)
 
@@ -285,7 +321,32 @@ class PotaMapRoot(tkinter.Tk):
         loc = self.combo_displayopts.get()
         print(loc)
         self.combo_displayopts_callback(event)
-        
+
+    def handle_check(self):
+        '''
+        Occurs when a users checks a box
+        '''
+        h = self.show_hunt_var.get()
+        a = self.show_actx_var.get()
+        u = self.show_unkn_var.get()
+        config['show_hunt_lbl'] = True if h == 1 else False
+        config['show_actx_lbl'] = True if a == 1 else False
+        config['show_unkn_lbl'] = True if u == 1 else False
+        self.map.delete_all_marker()
+        get_parks()
+        read_parks(self)
+
+    def accept_color_input(self, event):
+        '''
+        Occurs when users presses Enter or Leaves the color Entry widget
+        '''
+        val = self.color_var.get()
+        config['text_color'] = check_color_input(val)
+        self.map.delete_all_marker()
+        get_parks()
+        read_parks(self)
+
+
 if __name__ == "__main__":
     print(f"potamap version {VERSION}")
 
